3b84fce29602926b37c8b55d2988f3cc740545c8
KateKasiutich <kitkat.katekass@gmail.com>
2019-01-23 16:17:34 +0200

    initial commit

---
+++ assets/jsPart.js
@@ -0,0 +1,36 @@
+// check off specific Todos By Clicking
+$("ul").on("click", "li", function() {
+    if($(this).css("color") === "rgb(128, 128, 128)") {
+        $(this).css({
+            color: "#666",
+            textDecoration: "none"
+        })
+    }
+   else { 
+       $(this).css({
+        color: "gray",
+        textDecoration: "line-through"
+    });
+}
+});
+
+//click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//retrieve data from input
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13 && $(this).val() !== "") {
+        var text = $(this).val();
+        $('ul').append("<li><span><i class='fas fa-trash-alt'></i></span>" + text +  "</li>");
+        $(this).val("");
+    }
+});
+
+$(".fa-edit").click(function() {
+    $("input[type='text']").fadeToggle();
+})
